2fd4f3810951f412876f413a92e8d22892e6c427
Chris W <chris.xg.wang@gmail.com>
2026-06-21 18:55:17 +1000

    Switch map tiles to CartoDB for English labels

--- public/app.js
+++ public/app.js
@@ -743,9 +743,10 @@ async function init() {
     mapToggle.textContent = tt("hideMap");
     if (!map) {
       map = L.map("map").setView([31.5, 35.5], 7);
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: "&copy; OpenStreetMap contributors",
-        maxZoom: 18,
+      L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+        attribution: "&copy; <a href='https://carto.com/'>CARTO</a> &copy; <a href='https://www.openstreetmap.org/copyright'>OSM</a>",
+        maxZoom: 19,
+        subdomains: "abcd",
       }).addTo(map);
 
       for (const place of places) {
